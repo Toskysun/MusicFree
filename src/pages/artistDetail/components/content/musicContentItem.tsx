7f3b415ad3afddef0b45da5fe6c7b55c70ba76c5
Zencok <266256496+Zencok@users.noreply.github.com>
2026-04-12 11:46:31 +0800

    feat(artistDetail): show track duration in artist music list

--- src/pages/artistDetail/components/content/musicContentItem.tsx
+++ src/pages/artistDetail/components/content/musicContentItem.tsx
@@ -1,10 +1,20 @@
 import React from "react";
 import MusicItem from "@/components/mediaItem/musicItem";
+import timeformat from "@/utils/timeformat";
 
 interface IMusicContentProps {
     item: IMusic.IMusicItem;
 }
 export default function MusicContentItem(props: IMusicContentProps) {
     const { item } = props;
-    return <MusicItem musicItem={item} />;
+    return (
+        <MusicItem
+            musicItem={item}
+            titleTagSubText={
+                typeof item.duration === "number"
+                    ? timeformat(item.duration)
+                    : undefined
+            }
+        />
+    );
 }
