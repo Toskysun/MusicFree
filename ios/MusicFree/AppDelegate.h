cbcab4aa7508b56bbb3972ddabe766b744e52cc7
Zencok <266256496+Zencok@users.noreply.github.com>
2026-07-14 00:33:57 +0800

    fix(ios): migrate AppDelegate to Expo 57 for v0.7.11
    Replace EXAppDelegateWrapper with ExpoAppDelegate and ExpoReactNativeFactory.
    Bump release metadata to v0.7.11 and enable New Arch in iOS CI.

--- ios/MusicFree/AppDelegate.h
+++
@@ -1,9 +0,0 @@
-#import <RCTAppDelegate.h>
-#import <Expo/Expo.h>
-#import <UIKit/UIKit.h>
-
-@interface AppDelegate : EXAppDelegateWrapper
-
-@property (nonatomic, strong, readonly) UIWindow *window;
-
-@end
